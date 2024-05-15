bcf1c8745864c46e5d713182a4db7f769149ec11
Tamerlan13101975 <166745761+Tamerlan13101975@users.noreply.github.com>
2024-05-15 23:41:57 +0300

    new image code

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Тестовый проект")
 
+
+image = pygame.image.load("image.py.png")
+image_rect = image.get_rect()
+
+
+
+
 run = True
 while run:
     for event in pygame.event.get():
@@ -12,6 +19,7 @@ while run:
             run = False
 
     screen.fill((0, 0, 0))
+    screen.blit(image, image_rect)
     pygame.display.flip()
 
 
